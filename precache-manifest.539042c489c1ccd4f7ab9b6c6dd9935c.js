e13ebf65377ffee2732d2ac01549dbe4009a0e07
RakshithRAcharya <rakshithacharya1030@gmail.com>
2020-07-01 11:30:44 +0530

    Updates

---
+++ precache-manifest.539042c489c1ccd4f7ab9b6c6dd9935c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b4c8d251f89467708543c14691d7b7",
+    "url": "/SimpleWeatherApp/index.html"
+  },
+  {
+    "revision": "7e16903f41c8940c111b",
+    "url": "/SimpleWeatherApp/static/css/main.e9ceb523.chunk.css"
+  },
+  {
+    "revision": "25be97d2b3735b86c31f",
+    "url": "/SimpleWeatherApp/static/js/2.22c54a1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SimpleWeatherApp/static/js/2.22c54a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e16903f41c8940c111b",
+    "url": "/SimpleWeatherApp/static/js/main.727fd753.chunk.js"
+  },
+  {
+    "revision": "725c9bcdd56654bb6b41",
+    "url": "/SimpleWeatherApp/static/js/runtime-main.22638aaa.js"
+  }
+]);
